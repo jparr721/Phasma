fb27964354a251c8780b0feadc0735c6b762bb1f
Jarred Parr <jparr721@gmail.com>
2022-02-22 09:09:25 -0700

    add better memory pressure detection

--- p1/nn/cnn.py
+++ p1/nn/cnn.py
@@ -90,12 +90,19 @@ def make_model():
 
 def poll_ram():
     while True:
-        sleep(3)
-        usage = psutil.virtual_memory().active * 1e-9
+        sleep(1)
 
-        if usage > 12:
-            logger.error("Memory limit reached, killing process")
-            os._exit(1)
+        try:
+            used = int(
+                os.popen("free --giga --total | awk '/^Total:/ {print $3}'").read()
+            )
+
+            if used > 29:
+                logger.error("Memory limit reached, killing process")
+                logger.warning("Memory will not be cleaned up, I suggest a reboot")
+                os._exit(1)
+        except Exception as e:
+            logger.error(f"Error getting memory reading! This is dangerous! {e}")
 
 
 def train_model(model):
